036e26efc3555d1fc4b484d99f723c71ceba1177
zachrdz <zjrodr0709@hotmail.com>
2016-03-16 15:45:48 -0500

    adjust registration link on sign in page

--- signup.php
+++ signup.php
@@ -18,19 +18,6 @@ include_once 'includes/functions.php';
     <link rel="stylesheet" href="css/app.css" type="text/css" />
     <script type="text/JavaScript" src="js/sha512.js"></script>
     <script type="text/JavaScript" src="js/forms.js"></script>
-
-    <style>
-        #bg {
-            position: fixed;
-            top: 0;
-            left: 0;
-
-            /* Preserve aspet ratio */
-            min-width: 100%;
-            min-height: 100%;
-        }
-    </style>
-
     <!--[if lt IE 9]>
     <script src="js/ie/html5shiv.js"></script>
     <script src="js/ie/respond.min.js"></script>
@@ -44,9 +31,8 @@ if (!empty($error_msg)) {
 }
 ?>
 <section id="content" class="m-t-lg wrapper-md animated fadeInDown">
-    <img src="images/unbounce.png" id="bg" alt="">
     <div class="container aside-xl">
-        <a class="navbar-brand block" href="index.html"><span class="h1 font-bold" style="color:#ddd;">Rap.Haus</span></a>
+        <a class="navbar-brand block" href="index.html"><span class="h1 font-bold">Rap.Haus</span></a>
         <section class="m-b-lg">
             <header class="wrapper text-center">
                 <!--<strong>Sign up</strong>-->
