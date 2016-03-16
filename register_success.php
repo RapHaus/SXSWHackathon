4ed89b36fd7ed6432a0239085bc4a66f06c9b269
zachrdz <zjrodr0709@hotmail.com>
2016-03-16 13:42:26 -0500

    setup new registration page

--- register_success.php
+++ register_success.php
@@ -5,8 +5,14 @@
     <title>Secure Login: Registration Success</title>
     <link rel="stylesheet" href="styles/main.css" />
 </head>
-<body>
+<body onload="redirect();">
+<script>
+    function redirect(){
+        alert("Registration Successful!");
+        <?php header('Location: http://rap.haus/signin.php'); ?>
+    }
+</script>
 <h1>Registration successful!</h1>
-<p>You can now go back to the <a href="index.php">login page</a> and log in</p>
+<p>You can now go back to the <a href="signin.php">login page</a> and log in</p>
 </body>
 </html>
